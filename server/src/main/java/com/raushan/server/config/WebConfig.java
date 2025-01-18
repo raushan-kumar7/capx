08a1e4de22895ec59e2b102c5198ce5e45b9eaac
Raushan Kumar <raushan911721@gmail.com>
2025-01-19 00:16:32 +0530

    Fix bugs in WebConfig.java file

--- server/src/main/java/com/raushan/server/config/WebConfig.java
+++ server/src/main/java/com/raushan/server/config/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.lang.NonNull;  // Import NonNull annotation
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -18,13 +18,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-<<<<<<< HEAD
             .allowedOrigins(corsLocalUrl, corsDeployUrl)
             .allowedHeaders("*")
-=======
-            .allowedOrigins("https://capx-7oin.onrender.com")
-            //.allowedOrigins("http://localhost:5173")
->>>>>>> 1cfe6b86f7519058d908f612e319c5b1fb9307f1
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(true);
     }
